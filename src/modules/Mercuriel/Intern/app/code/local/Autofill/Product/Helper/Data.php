e2025c33d8dfdad44be96d05acea5adf8d6d8eb0
Phong Phan <pthphong2103@gmail.com>
2016-11-04 23:58:43 +0700

    Modified Composer.json and added autofill product module

---
+++ src/modules/Mercuriel/Intern/app/code/local/Autofill/Product/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by Phong Phan.
+ * Copyright Mercuriel - 2016 
+ * Date: 04/11/2016
+ * Time: 23:28
+ */ 
+class Autofill_Product_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
