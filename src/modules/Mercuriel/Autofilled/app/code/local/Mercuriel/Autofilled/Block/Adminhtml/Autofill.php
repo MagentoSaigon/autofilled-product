9337ef658936880d255e464bcc68d1a40805b3ff
LAPTOP-HOANG\User <maihoang626@gmail.com>
2016-11-18 18:54:48 +0700

    181116 - Create adminhtml grid

--- src/modules/Mercuriel/Autofilled/app/code/local/Mercuriel/Autofilled/Block/Adminhtml/Autofill.php
+++ src/modules/Mercuriel/Autofilled/app/code/local/Mercuriel/Autofilled/Block/Adminhtml/Autofill.php
@@ -3,9 +3,13 @@ class Mercuriel_Autofilled_Block_Adminhtml_Autofill extends Mage_Adminhtml_Block
     public function __construct()
     {
         $this->_controller = "adminhtml_autofilled";
+
         $this->_blockGroup = "mercuriel_autofilled";
+
         $this->_headerText = Mage::helper('mercuriel_autofilled')->__('Attribute Auto-Fill Set');
-        $this->_addButtonLabel = Mage::helper('mercuriel_autofilled')->__('Add New Auto-fill Set');
+
         parent::__construct();
+        
+        $this->_addButtonLabel = Mage::helper('mercuriel_autofilled')->__('Add New Auto-fill Set');
     }
 }
--- src/modules/Mercuriel/Autofilled/app/code/local/Mercuriel/Autofilled/controllers/Adminhtml/AutofilledController.php
+++ src/modules/Mercuriel/Autofilled/app/code/local/Mercuriel/Autofilled/controllers/Adminhtml/AutofilledController.php
@@ -4,8 +4,15 @@ class Mercuriel_Autofilled_Adminhtml_AutofilledController extends Mage_Adminhtml
         $this->_title($this->__('Autofill Set'));
         $this->loadLayout()
         ->_setActiveMenu('catalog');
+        $this->_addContent($this->getLayout()->createBlock('mercuriel_autofilled/adminhtml_autofill'));
         $this->renderLayout();
-        return $this;
+    }
+
+    public function gridAction(){
+        $this->loadLayout();
+        $this->getReponse()->setBody(
+            $this->getLayout()->createBlock('mercuriel_autofilled/adminhtml_autofill_grid')->toHtml()
+        );
     }
 
     protected function _initAction(){
