b47c73d642ab7a7924a8eeef8a68855db9c883eb
tuan do <dptuan.itiu@gmail.com>
2016-11-21 23:32:35 +0700

    Remove some files

--- bin/codesniff.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-files=$(git log --name-only HEAD^..HEAD | grep -F ".ph")
-
-if [ ${#files} -gt 0 ]; then
-    phpcs --standard=Ecg -n $files
-else
-    echo "no PHP files modified"
-fi
--- bin/modman/modman.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sScript="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/modman.php";
-php "$sScript" "$@"; 
